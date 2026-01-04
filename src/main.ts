38270bc77ad83c6ecce7b7898267ae13916785b7
Kevin Katzer <kevinka999@gmail.com>
2026-01-04 09:54:06 -0300

    chore/fix: changing port and uncommenting code

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ZodValidationPipe } from 'nestjs-zod';
+import cookieParser from 'cookie-parser';
 
 async function setupSwagger(app: INestApplication) {
   if (process.env.NODE_ENV !== 'development') return;
@@ -19,8 +21,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await setupSwagger(app);
 
-  // app.use(cookieParser());
-  // app.useGlobalPipes(new ZodValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+  app.useGlobalPipes(new ZodValidationPipe());
+  await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
